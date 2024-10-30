b887bc74acf8cb367abe892e7368058b64975b01
Francisco Javier Moreno <fjavier.moreno@outlook.com>
2024-10-30 01:46:42 +0100

    Exercise 3.7

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+app.use(morgan('tiny'))
 
 let phonebook = [
 	{ 
@@ -25,6 +27,16 @@ let phonebook = [
 		"number": "39-23-6423122"
 	}
 ]
+
+morgan(function (tokens, request, response) {
+	return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms'
+  ].join(' ')
+})
 
 app.get('/api/persons', (request, response) => {
 	response.json(phonebook)
@@ -57,8 +69,6 @@ const generateId = () => {
 app.post('/api/persons', (request, response) => {
 	const body = request.body
 
-	console.log(body)
-
 	if (!body.name) {
 		return response.status(400).json({ error: 'Field \'name\' is mandatory'})
 	} else if (!body.number) {
@@ -84,6 +94,12 @@ app.delete('/api/persons/:id', (request, response) => {
 
 	response.status(204).end()
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
